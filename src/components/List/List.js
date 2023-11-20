fdb5b0ce8536615a47001259346fa66449efe9d2
Piotr Węgłowski <weglowskipiotr@gmail.com>
2023-11-20 22:32:06 +0100

    Fix bug in columnsRedux

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,7 +3,7 @@ import Column from './../Column/Column';
 import ColumnForm from './../ColumnForm/ColumnForm';
 import { useSelector } from 'react-redux/es/hooks/useSelector';
 import { getListById } from '../../redux/listsRedux';
-import { getColumnsByList } from '../../redux/store';
+import { getColumnsByList } from '../../redux/columnsRedux';
 import { useParams } from 'react-router';
 import SearchForm from '../SearchForm/SearchForm';
 import { Navigate } from 'react-router-dom';
